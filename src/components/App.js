0775c87c3732beef7e206be2eb29206a457dfab8
jruel <jruel@sandstormdesign.com>
2019-03-20 22:23:38 -0500

    set up page styles and icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+// import IconGithub from './components/icons/IconGithub';
+import Icon from './Icon';
 
 class App extends Component {
   constructor(props) {
@@ -7,9 +9,35 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Test</h1>
-      </div>
+      <main className="main">
+        {/* <div className="links">
+          <a href="">
+            <Icon icon="github"/>
+          </a>
+
+          <a href="">
+            <Icon icon="linkedin"/>
+          </a>
+        </div> */}
+        <div className="intro">
+
+          <div className="intro-left">
+            <div className="hello">
+              Hi, I'm
+            </div>
+            <div className="name">
+              Joe Ruel
+            </div>
+          </div>
+          <div className="intro-right">
+            <div className="bio">
+            I'm a Chicago-based full stack developer.
+            Check me out on <a target="_blank" href="https://github.com/jnruel">Github</a> and <a target="_blank" href="https://www.linkedin.com/in/joeruel/">LinkedIn</a>.
+            </div>
+          </div>
+        </div>
+      </main>
+
     )
   }
 }
