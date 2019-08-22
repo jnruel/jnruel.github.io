2c3b320c7fd2ce7463b4c4dbc119d4f5542fa140
jruel <jruel@sandstormdesign.com>
2019-08-22 13:49:25 -0500

    update npm dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
