b587fdba7697accebc0ab88d76c7ac911e1b9678
jruel <jruel@sandstormdesign.com>
2020-03-12 21:05:19 -0500

    minor fixes and style updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,24 +9,24 @@ class Home extends Component {
 
   render() {
     return (
-      <div class="page-home">
-        {/* <div className="content-main"> */}
-          <div className="intro">
-            <div className="intro-left">
-              <div className="hello">
-                Hi, I'm
-              </div>
-              <div className="name">
-                Joe Ruel
-              </div>
+      <div className="page page__home">
+        <div className="intro">
+          <div className="intro-left">
+            <div className="hello">
+              Hi, I'm
             </div>
-            <div className="intro-right">
-              <div className="bio">
-              I'm a web developer based in Chicago. <span>Take a look at my <Link to="/resume">r&eacute;sum&eacute;</Link> or check me out on <a target="_blank" href="https://github.com/jnruel" rel="noopener">Github</a> and <a target="_blank" href="https://www.linkedin.com/in/joeruel/" rel="noopener">LinkedIn</a>.</span>
-              </div>
+            <div className="name">
+              Joe Ruel
             </div>
           </div>
-        {/* </div> */}
+          <div className="intro-right">
+            <div className="bio">
+              I'm a web developer based in Chicago.
+              <span>Take a look at my <Link to="/resume">r&eacute;sum&eacute;</Link> or check me out on <a target="_blank" href="https://github.com/jnruel" rel="noopener">Github</a> and <a target="_blank" href="https://www.linkedin.com/in/joeruel/" rel="noopener">LinkedIn</a>.
+              </span>
+            </div>
+          </div>
+        </div>
         <div className="content-logo">
           <div className="logo-container">
             <Logo width="100%" height="100%"/>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,120 +8,123 @@ class Resume extends Component {
 
   render() {
     return (
-      <div className="page-resume">
-        <div className="grid">
+      <div className="page page__resume">
+        <Link className="print-hidden" to="/">&larr; Back to home</Link>
+        <div className="resume">
+          <div className="grid">
 
-          <div className="grid-item header">
-            <Link className="print-hidden" to="/">&larr; Back to home</Link>
-            <section>
-              <h1>Joe Ruel</h1>
-              <h2>Web Developer</h2>
-            </section>
-          </div>
+            <div className="grid-item header">
+              <section>
+                <h1>Joe Ruel</h1>
+                <h2>Web Developer</h2>
+              </section>
+            </div>
 
-          <div className="grid-item experiences">
-            <h2>Professional Experience</h2>
-            <section className="experience-item">
-              <h3>Full Stack Developer<br />Sandstorm Design, Chicago, IL<br />2016-19</h3>
-              <div className="experience-body">
-                <ul>
-                  <li>Promoted from front end developer in 2018</li>
-                  <li>Assist with implementation and iteration of internal patterns and devops tools</li>
-                  <li>Manage multiple projects and collaborate cross functionally to align clients’ needs and the agency’s deliverables</li>
-                  <li>Document processes to ensure better teamwork and efficiency, such as project developer documentation and requirements</li>
-                  <li>Collaborate with QA to determine a test plan and proactively fix bugs before delivering to clients</li>
-                  <li><em>Notable projects:</em>
-                    <ul>
-                      <li>Lead front end developer on a Hermes platinum award-winning site redesign for a national cleaning supply company</li>
-                      <li>Front and back end developer for an ecommerce integration into an existing natural products site</li>
-                      <li>Custom email digest system that collects activity over a period of time and notifies users</li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </section>
-            <section className="experience-item">
-              <h3>Web Developer<br />Brella Productions, Evanston, IL<br />2014-16</h3>
-              <div className="experience-body">
-                <ul>
-                  <li><em>Notable projects:</em> enterprise iOS event app, backend editor tool, rest API for mobile app</li>
-                  <li>Interned in summer of 2012</li>
-                </ul>
-              </div>
-            </section>
-            <section className="experience-item">
-              <h3>Developer, Summer 2013<br />Bradley University, Peoria IL</h3>
-              <div className="experience-body">
-                <ul>
-                  <li>Primary developer for Dapper, made in cooperation with a hospital to improve outcomes of patients with Type II Diabetes</li>
-                </ul>
-              </div>
-            </section>
-            <h2>Education</h2>
-            <section className="experience-item">
-              <h3>Bradley University, Peoria IL</h3>
-              <ul>
-                <li>B.S. in interactive media, game design concentration</li>
-                <li>Minors in computer science and studio art</li>
-              </ul>
-            </section>
-          </div>
-
-          <aside className="grid-item sidebar">
-            <h2>Skills</h2>
-            <div className="section-container">
-              <section className="sidebar-section">
-                <h3>Languages</h3>
-                <ul>
-                  <li>JavaScript</li>
-                  <li>PHP</li>
-                  <li>CSS/Sass</li>
-                  <li>HTML</li>
-                  <li>Python</li>
-                  <li>Ruby</li>
-                </ul>
+            <div className="grid-item experiences">
+              <h2>Professional Experience</h2>
+              <section className="experience-item">
+                <h3>Full Stack Developer<br />Sandstorm Design, Chicago, IL<br />2016-19</h3>
+                <div className="experience-body">
+                  <ul>
+                    <li>Promoted from front end developer in 2018</li>
+                    <li>Assist with implementation and iteration of internal patterns and devops tools</li>
+                    <li>Manage multiple projects and collaborate cross functionally to align clients’ needs and the agency’s deliverables</li>
+                    <li>Document processes to ensure better teamwork and efficiency, such as project developer documentation and requirements</li>
+                    <li>Collaborate with QA to determine a test plan and proactively fix bugs before delivering to clients</li>
+                    <li><em>Notable projects:</em>
+                      <ul>
+                        <li>Lead front end developer on a Hermes platinum award-winning site redesign for a national cleaning supply company</li>
+                        <li>Front and back end developer for an ecommerce integration into an existing natural products site</li>
+                        <li>Custom email digest system that collects activity over a period of time and notifies users</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
               </section>
-              <section className="sidebar-section">
-                <h3>Tools/Utilities</h3>
-                <ul>
-                  <li>Git</li>
-                  <li>Gulp</li>
-                  <li>Webpack</li>
-                  <li>Babel</li>
-                  <li>Vagrant NPM/Composer/Bundler</li>
-                  <li>NVM/RVM</li>
-                  <li>Bash/Zsh</li>
-                  <li>Editorconfig</li>
-                  <li>ESLint</li>
-                </ul>
+              <section className="experience-item">
+                <h3>Web Developer<br />Brella Productions, Evanston, IL<br />2014-16</h3>
+                <div className="experience-body">
+                  <ul>
+                    <li><em>Notable projects:</em> enterprise iOS event app, backend editor tool, rest API for mobile app</li>
+                    <li>Interned in summer of 2012</li>
+                  </ul>
+                </div>
               </section>
-              <section className="sidebar-section">
-                <h3>Frameworks/Libraries</h3>
-                <ul>
-                  <li>Drupal</li>
-                  <li>WordPress</li>
-                  <li>Laravel</li>
-                  <li>Rails</li>
-                  <li>Bootstrap</li>
-                  <li>React</li>
-                  <li>Gatsby</li>
-                  <li>jQuery</li>
-                </ul>
+              <section className="experience-item">
+                <h3>Developer, Summer 2013<br />Bradley University, Peoria IL</h3>
+                <div className="experience-body">
+                  <ul>
+                    <li>Primary developer for Dapper, made in cooperation with a hospital to improve outcomes of patients with Type II Diabetes</li>
+                  </ul>
+                </div>
               </section>
-              <section className="sidebar-section">
-                <h3>Software</h3>
+              <h2>Education</h2>
+              <section className="experience-item">
+                <h3>Bradley University, Peoria IL</h3>
                 <ul>
-                  <li>Mac/Windows</li>
-                  <li>CentOS/Ubuntu</li>
-                  <li>LAMP Stack</li>
-                  <li>VSCode/Sublime</li>
-                  <li>Photoshop</li>
-                  <li>Illustrator</li>
-                  <li>InDesign</li>
+                  <li>B.S. in interactive media, game design concentration</li>
+                  <li>Minors in computer science and studio art</li>
                 </ul>
               </section>
             </div>
-          </aside>
+
+            <aside className="grid-item sidebar">
+              <h2>Skills</h2>
+              <div className="section-container">
+                <section className="sidebar-section">
+                  <h3>Languages</h3>
+                  <ul>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>CSS/Sass</li>
+                    <li>HTML</li>
+                    <li>Python</li>
+                    <li>Ruby</li>
+                  </ul>
+                </section>
+                <section className="sidebar-section">
+                  <h3>Tools/Utilities</h3>
+                  <ul>
+                    <li>Git</li>
+                    <li>Gulp</li>
+                    <li>Webpack</li>
+                    <li>Babel</li>
+                    <li>Vagrant </li>
+                    <li>NPM / Composer / Bundler</li>
+                    <li>NVM / RVM</li>
+                    <li>Bash / Zsh</li>
+                    <li>Editorconfig</li>
+                    <li>ESLint</li>
+                  </ul>
+                </section>
+                <section className="sidebar-section">
+                  <h3>Frameworks/Libraries</h3>
+                  <ul>
+                    <li>Drupal</li>
+                    <li>WordPress</li>
+                    <li>Laravel</li>
+                    <li>Rails</li>
+                    <li>Bootstrap</li>
+                    <li>React</li>
+                    <li>Gatsby</li>
+                    <li>jQuery</li>
+                  </ul>
+                </section>
+                <section className="sidebar-section">
+                  <h3>Software</h3>
+                  <ul>
+                    <li>Mac/Windows</li>
+                    <li>CentOS/Ubuntu</li>
+                    <li>LAMP Stack</li>
+                    <li>VSCode/Sublime</li>
+                    <li>Photoshop</li>
+                    <li>Illustrator</li>
+                    <li>InDesign</li>
+                  </ul>
+                </section>
+              </div>
+            </aside>
+          </div>
         </div>
       </div>
     )
