b44dd61afe7709f4996da97664e86af4ce95b3fd
jruel <jruel@sandstormdesign.com>
2020-04-04 14:58:35 -0500

    homepage content and styling updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,26 +11,31 @@ class Home extends Component {
       <div className="page page__home">
         <div className="intro">
           <div className="intro-left">
-            <div className="hello">
-              Hi, I'm
-            </div>
-            <div className="name">
-              Joe Ruel
+            <div className="logo-container">
+              <Logo width="100%" height="100%"/>
             </div>
           </div>
           <div className="intro-right">
             <div className="bio">
-              I'm a web developer based in Chicago. <span>Check me out on <a target="_blank" href="https://github.com/jnruel" rel="noopener">Github</a> and <a target="_blank" href="https://www.linkedin.com/in/joeruel/" rel="noopener">LinkedIn</a>.</span>
+              <p>
+                <strong>Hi, I'm Joe Ruel.&nbsp;</strong>
+                <span>I enjoy making things with code.&nbsp;</span>
+              </p>
+              <ul className="links">
+                <code className="comment">// links</code>
+                <li><a href="">Github</a></li>
+                <li><a href="">Codepen</a></li>
+                <li><a href="">LinkedIn</a></li>
+              </ul>
+              <ul>
+                <code className="comment">// contact</code>
+                <li className="email">joe [at] jruel [dot] com</li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="content-logo">
-          <div className="logo-container">
-            <Logo width="100%" height="100%"/>
-          </div>
-        </div>
       </div>
-    )
+    );
   }
 }
 
