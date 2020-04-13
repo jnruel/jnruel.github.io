b51f7db8bcec016c02e735b0519490fad2dca524
Joe <jnruel@users.noreply.github.com>
2020-04-13 18:34:41 -0500

    Content/layout updates (#2)
    * homepage content and styling updates
    
    * more homepage updates, added new icons
    
    * color updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Logo from './Logo';
+import IconLink from './IconLink';
+import Icon from './Icon';
 
 class Home extends Component {
   constructor(props) {
@@ -11,26 +13,33 @@ class Home extends Component {
       <div className="page page__home">
         <div className="intro">
           <div className="intro-left">
-            <div className="hello">
-              Hi, I'm
-            </div>
-            <div className="name">
-              Joe Ruel
+            <div className="logo-container">
+              <Logo width="100%" height="100%"/>
             </div>
           </div>
           <div className="intro-right">
             <div className="bio">
-              I'm a web developer based in Chicago. <span>Check me out on <a target="_blank" href="https://github.com/jnruel" rel="noopener">Github</a> and <a target="_blank" href="https://www.linkedin.com/in/joeruel/" rel="noopener">LinkedIn</a>.</span>
+              <p>
+                <strong>Hi, I'm Joe.&nbsp;</strong>
+                <span>I enjoy making things with code.&nbsp;</span>
+              </p>
+              <div className="links-container">
+                <ul className="links">
+                  <code className="comment">// links</code>
+                  <li><IconLink url="https://github.com/jnruel" text="Github" icon="github"/></li>
+                  <li><IconLink url="https://codepen.io/jruel" text="Codepen" icon="codepen"/></li>
+                  <li><IconLink url="https://www.linkedin.com/in/joeruel/" text="LinkedIn" icon="linkedin" /></li>
+                </ul>
+              </div>
+              <ul className="contact">
+                <code className="comment">// contact</code>
+                <li style={{display: 'flex'}} className="email"><Icon icon="email" /> joe [at] jruel [dot] com</li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="content-logo">
-          <div className="logo-container">
-            <Logo width="100%" height="100%"/>
-          </div>
-        </div>
       </div>
-    )
+    );
   }
 }
 
