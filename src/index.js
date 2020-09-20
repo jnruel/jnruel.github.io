b95350b708cdeef3f065ae9bd2ed39b6b99d30ad
jruel <jruel@sandstormdesign.com>
2020-09-20 09:24:07 -0500

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import App from './components/App'
 
 import './styles/main.scss'
 
 const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-}
-else {
-  render(<App />, rootElement);
-}
+
+render(<App />, rootElement);
